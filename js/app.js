dd06e9e530add1a83f5722a17daa40c3edadc508
tt <tt@Mac.lan>
2026-04-07 15:58:49 -0400

    main

---
+++ js/app.js
@@ -0,0 +1,36 @@
+// app.js
+import { buildSidebar } from "./core/build-sidebar.js"
+import { initInjectContentListeners } from "./core/inject-content.js"
+
+import { initDarkMode } from "./dark-mode.js"
+import { keyboardNav } from "./nav/keyboard-nav.js"
+import { initToggleSideBar } from "./ui/toggle-sidebar.js"
+import { initCopyCodes } from "./copy-code.js"
+
+document.addEventListener("DOMContentLoaded", initApp)
+
+async function initApp() {
+
+    // Build sidebar first
+    await buildSidebar()
+
+    // Sidebar navigation
+    initInjectContentListeners()
+
+    // UI features
+    initToggleSideBar()
+    initDarkMode()
+    initCopyCodes()
+
+    // global keyboard nav
+    setupGlobalListeners()
+
+}
+
+function setupGlobalListeners() {
+    document.addEventListener("keydown", e => {
+        const tag = e.target.tagName.toLowerCase()
+        if(tag === 'input' || tag === 'textarea') return
+        keyboardNav(e)
+    })
+}
